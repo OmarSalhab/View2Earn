eddee373023b49a7082d33f084082a3be37f8738
OmarSalhab <omarsalhb24@gmail.com>
2025-04-20 03:12:24 +0300

    fix: Correct bottom positioning in Toast component for better visibility

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -5,7 +5,7 @@ const Toast = ({ title }) => {
 		return (
 			<div className="fixed inset-0">
 				<div
-					className={`flex justify-center gap-3 items-center absolute sm:bottom-10 bottom-3 sm:left-6 left-1 transition-transform h-[9%] sm:w-[21%] w-full p-2 ${
+					className={`flex justify-center gap-3 items-center absolute sm:bottom-10 bottom-20 sm:left-6 left-1 transition-transform h-[9%] sm:w-[21%] w-full p-2 ${
 						title === "Submitted Succssfully" ? "bg-green-600" : "bg-red-600"
 					} text-white font-medium`}
 				>
